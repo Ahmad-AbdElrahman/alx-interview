d332b35f02af770d126bab8a3227f7d39447ed32
Ahmed <ahmed@ahmed>
2024-07-25 20:44:16 +0300

    task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if isinstance(n, int):
+        if (n > 0):
+            pascal_list = [[1]]
+            for i in range(1, n):
+                new_list = []
+                for j in range(i + 1):
+                    if j == 0 or j == i:
+                        new_list.append(1)
+                    else:
+                        new_list.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
+                pascal_list.append(new_list)
+            return pascal_list
+        else:
+            return []
+    else:
+        print("Please enter an integer")
+

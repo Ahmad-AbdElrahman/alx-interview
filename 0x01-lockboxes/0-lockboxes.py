d194ff72ef7459838de244949842923f29374753
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-08-02 00:45:53 +0300

    solving task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,20 +9,15 @@ def canUnlockAll(boxes):
     solving Lockbox project
     """
     n = len(boxes)
-    if n == 0:
-        return False
-    
     open_boxes = [boxes[0]]
     for box in open_boxes:
         for key in box:
-            for j in range(len(boxes)):
+            for j in range(n):
                 if boxes[j] not in open_boxes:
                     if key == j or len(boxes[j]) == 0:
                         open_boxes.append(boxes[j])
-                    j = j+1
                 else:
                     continue
-    if (len(open_boxes) == len(boxes)):
+    if len(open_boxes) == n:
         return True
-    else:
-        return False
+    return False
